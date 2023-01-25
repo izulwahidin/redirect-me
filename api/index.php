0010b640172500f7dc81d5d828b5bba5c843f9e5
Izul Wahidin <iam@wahidin.eu.org>
2023-01-26 00:15:54 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,20 @@
-<?php 
-print_r($_SERVER);
+<?php
+define("default", "https://Twitter.com");
+
+
+$redirs = [
+  "ID" => "https://fb.com",
+  "SG" => "https://yahoo.com",
+]
+
+
+$srv = (object) $_SERVER;
+
+// Redirect to matched country
+foreach (@$redirs ?? [] as $c => $url){
+  $rx = "/$c/i";
+  if (strcasecmp($c,$srv->HTTP_X_VERCEL_IP_COUNTRY) === false) header("Location: ". $url)
+}
+
+// Redirect to default url
+header("Location: ". default)
